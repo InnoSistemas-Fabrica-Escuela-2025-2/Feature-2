3dfcb900ad27be19c8e3a95b8d161850d02ba8a8
Sebastian Renteria <sebastian.renteria@udea.edu.co>
2025-11-06 16:09:25 -0500

    tests updated

--- backend/innosistemas/src/test/java/com/udea/innosistemas/innosistemas/InnosistemasApplicationTests.java
+++ backend/innosistemas/src/test/java/com/udea/innosistemas/innosistemas/InnosistemasApplicationTests.java
@@ -3,9 +3,11 @@ package com.udea.innosistemas.innosistemas;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@TestPropertySource(locations = "classpath:application-test.properties")
 class InnosistemasApplicationTests {
 
 	@Test
